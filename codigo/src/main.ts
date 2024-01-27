0482955388aac9a2cf5d4a0051a798b247d271a9
libialany <elibialany@gmail.com>
2024-01-27 19:36:04 +0000

    feat: adding correcciones docker

--- codigo/src/main.ts
+++ codigo/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ConfigService } from '@nestjs/config'
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import express from 'express'
 import session from 'express-session'
 import passport from 'passport'
@@ -46,9 +45,6 @@ const bootstrap = async () => {
   const configService = app.get(ConfigService)
 
   // swagger
-  if (configService.get('NODE_ENV') !== 'production') {
-    createSwagger(app)
-  }
 
   await SessionAppDataSource.initialize()
 
@@ -99,17 +95,5 @@ const bootstrap = async () => {
   })
 }
 
-function createSwagger(app: INestApplication) {
-  const options = new DocumentBuilder()
-    .setTitle(SWAGGER_API_NAME)
-    .setDescription(SWAGGER_API_DESCRIPTION)
-    .setVersion(SWAGGER_API_CURRENT_VERSION)
-    .addServer(`http://${process.env.BACKEND_HOSTNAME}:${process.env.PORT}/api/`)
-    .addBearerAuth()
-    .build()
-
-  const document = SwaggerModule.createDocument(app, options)
-  SwaggerModule.setup(SWAGGER_API_ROOT, app, document)
-}
 
 bootstrap()
